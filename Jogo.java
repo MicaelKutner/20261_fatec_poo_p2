845e04908096c7c7ff8ccd8de09cf7fff3e4c676
Micael <mpkutner@gmail.com>
2026-05-24 21:46:44 -0300

    fix(p2): incluido duelo no loop

--- Jogo.java
+++ Jogo.java
@@ -6,10 +6,23 @@ public class Jogo {
     public static void main(String[] args) throws Exception{
         Personagem p1 = new Cacador("Jarbas");         
         Personagem p2 = new Bardo("Tico Trovador", 3, 8, 8);
+        var gerador = new Random();
         //boolean campeaoDeclarado = false;
         while(true){
             p1.realizarAcaoAleatoria();
-            p2.realizarAcaoAleatoria();        
+            p2.realizarAcaoAleatoria();
+            var horaDoDuelo = gerador.nextInt(1, 3);
+            if(!p1.estaMorto() && !p2.estaMorto()){
+                
+                switch(horaDoDuelo){
+                    case 1:
+                        p1.duelar(p2);
+                        break;
+                    case 2:
+                        p2.duelar(p1);
+                        break;
+                }
+            }       
                         
             System.out.println(p1);
             System.out.println(p2);
